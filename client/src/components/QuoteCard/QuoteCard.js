7fd91c30431346cdff5562b1b7b8b3517b5059a9
Nick Ramsay <NickRamsay1989@gmail.com>
2022-12-24 22:11:22 +1100

    Made quote card into component

---
+++ client/src/components/QuoteCard/QuoteCard.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from "react";
+import editIcon from "../../images/outline_edit_white_24dp.png";
+
+const QuoteCard = (props) => {
+    let stock = props.stock
+    let userID = props.userID;
+    let updatePortfolio = props.updatePortfolio;
+
+    return (
+        <div className="card mb-3">
+            <div className="card-body">
+                <h5 className="card-title row">
+                    <div className="col-md-12">
+                        <a href={"https://finviz.com/quote.ashx?t=" + stock.symbol + "&ty=l&ta=0&p=m&tas=0"} target="_blank">{stock.quote.companyName + " (" + stock.symbol + ")"}</a>
+                        {userID !== undefined && userID !== "" ?
+                            <span><img
+                                className="ml-3 text-icon"
+                                src={editIcon}
+                                alt="editIcon"
+                                data-bs-toggle="modal" data-bs-target={"#" + stock.symbol + "editModal"}
+                            /></span> : ""
+                        }
+                    </div>
+                    {/*Start: Edit Symbol Modal*/}
+                    <div class="modal fade" id={stock.symbol + "editModal"} tabindex="-1" role="dialog" aria-labelledby={stock.symbol + "editModalLabel"} aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id={stock.symbol + "editModalLabel"}>Edit {stock.quote.companyName + " (" + stock.symbol + ")"}</h5>
+                                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <form>
+                                        <select id={stock.symbol + "PortfolioStatusInput"} class="form-select" aria-label="Default select example">
+                                            <option value="-" selected>-</option>
+                                            <option value="watch">Watch</option>
+                                            <option value="own">Own</option>
+                                            <option value="hold">Hold</option>
+                                            <option value="icebox">Icebox</option>
+                                            <option value="speculative">Speculative</option>
+                                        </select>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary" onClick={() => updatePortfolio(stock.symbol, userID)}>Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/*Start: Edit Symbol Modal*/}
+                </h5>
+                <div className="row">
+                    {stock.fundamentals.country && stock.fundamentals.country !== null ?
+                        <div className="col-md-12">
+                            <span><strong>Sector:</strong> {stock.fundamentals.sector} | <strong>Industry:</strong> {stock.fundamentals.industry} | <strong>Country:</strong> {stock.fundamentals.country}</span>
+                        </div>
+                        : ""}
+                </div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <p><strong>Price: </strong> ${stock.quote.latestPrice.toFixed(2)}</p>
+                    </div>
+                    <div className="col-md-4">
+                        <p><strong>Target Price: </strong> ${Number(stock.fundamentals['Target Price']).toFixed(2)}</p>
+                    </div>
+                    <div className="col-md-4">
+                        {stock.fundamentals['Target Price'] >= stock.quote.latestPrice ?
+                            <p className="badge badge-success py-1 px-1">{((1 - (stock.quote.latestPrice / stock.fundamentals['Target Price'])) * 100).toFixed(2) + "% Undervalued"}</p>
+                            : <p className="badge badge-danger py-1 px-1">{(((stock.quote.latestPrice / stock.fundamentals['Target Price']) - 1) * 100).toFixed(2) + "% Overvalued"}</p>}
+                    </div>
+                </div>
+                <div className="progress bg-dark">
+                    <div className="progress-bar" role="progressbar" style={{ width: Math.round(stock.quote.latestPrice / stock.quote.week52High * 100) + "%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">{"$" + stock.quote.latestPrice}</div>
+                </div>
+                <div className="row mt-2">
+                    <div className="col-md-4">
+                        <p><strong>Index: </strong>{stock.symbolData.exchangeName}</p>
+                    </div>
+                    <div className="col-md-4">
+                        <p><strong>P/E: </strong>{stock.quote.peRatio}</p>
+                    </div>
+                    <div className="col-md-4">
+                        <p><strong>Profit Margin: </strong>{Number(stock.fundamentals['Profit Margin (%)']).toFixed(2)}%</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <p><strong>Market Cap: </strong>${(stock.quote.marketCap / 1000000000).toFixed(2)} billion</p>
+                    </div>
+                    <div className="col-md-4">
+                        <p><strong>Debt/Equity: </strong>{(stock.fundamentals['Debt/Eq'])} </p>
+                    </div>
+                    <div className="col-md-4">
+                        <p><strong>Price-to-Sales: </strong>{(stock.fundamentals['P/S'])} </p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <p><strong>Price-to-Book: </strong>{(stock.fundamentals['P/B'])} </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default QuoteCard;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,14 +4,12 @@ import BarLoader from "react-spinners/BarLoader";
 import { useInput } from "../../sharedFunctions/sharedFunctions";
 import API from "../../utils/API";
 import moment from "moment";
-import logo from "../../../src/logo.svg";
-import GithubLogo from "../../images/github_logos/GitHub_Logo_White.png";
-import mongoLogo from "../../images/mongo_logo.png";
-import editIcon from "../../images/outline_edit_white_24dp.png";
 import expandMoreIcon from "../../images/outline_expand_more_white_24dp.png";
 import expandLessIcon from "../../images/outline_expand_less_white_24dp.png";
 import { sha256 } from 'js-sha256';
 import "./style.css";
+
+import QuoteCard from "../../components/QuoteCard/QuoteCard";
 
 const Home = () => {
   var [valueSearchData, setValueSearchData] = useState([]);
@@ -142,7 +140,7 @@ const Home = () => {
   //END: User Account Creation Functions
 
   const findPortfolio = (user) => {
-    API.findPortfolio(user).then(res => {console.log(res.data); setPortfolio(portfolio => res.data.portfolio)});
+    API.findPortfolio(user).then(res => { console.log(res.data); setPortfolio(portfolio => res.data.portfolio) });
   };
 
   const updatePortfolio = (symbol, userID) => {
@@ -568,103 +566,7 @@ const Home = () => {
             </div>
             {!loading ?
               valueSearchData.map((stock, i) =>
-                <div className="card mb-3">
-                  <div className="card-body">
-                    <h5 className="card-title row">
-                      <div className="col-md-12">
-                        <a href={"https://finviz.com/quote.ashx?t=" + stock.symbol + "&ty=l&ta=0&p=m&tas=0"} target="_blank">{stock.quote.companyName + " (" + stock.symbol + ")"}</a>
-                        {userID !== undefined && userID !== "" ?
-                          <span><img
-                            className="ml-3 text-icon"
-                            src={editIcon}
-                            alt="editIcon"
-                            data-bs-toggle="modal" data-bs-target={"#" + stock.symbol + "editModal"}
-                          /></span> : ""
-                        }
-                      </div>
-                      {/*Start: Edit Symbol Modal*/}
-                      <div class="modal fade" id={stock.symbol + "editModal"} tabindex="-1" role="dialog" aria-labelledby={stock.symbol + "editModalLabel"} aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                              <h5 class="modal-title" id={stock.symbol + "editModalLabel"}>Edit {stock.quote.companyName + " (" + stock.symbol + ")"}</h5>
-                              <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                              </button>
-                            </div>
-                            <div class="modal-body">
-                              <form>
-                                <select id={stock.symbol + "PortfolioStatusInput"} class="form-select" aria-label="Default select example">
-                                  <option value="-" selected>-</option>
-                                  <option value="watch">Watch</option>
-                                  <option value="own">Own</option>
-                                  <option value="hold">Hold</option>
-                                  <option value="icebox">Icebox</option>
-                                  <option value="speculative">Speculative</option>
-                                </select>
-                              </form>
-                            </div>
-                            <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                              <button type="button" class="btn btn-primary" onClick={() => updatePortfolio(stock.symbol, userID)}>Save changes</button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      {/*Start: Edit Symbol Modal*/}
-                    </h5>
-                    <div className="row">
-                      {stock.fundamentals.country && stock.fundamentals.country !== null ?
-                        <div className="col-md-12">
-                          <span><strong>Sector:</strong> {stock.fundamentals.sector} | <strong>Industry:</strong> {stock.fundamentals.industry} | <strong>Country:</strong> {stock.fundamentals.country}</span>
-                        </div>
-                        : ""}
-                    </div>
-                    <div className="row">
-                      <div className="col-md-4">
-                        <p><strong>Price: </strong> ${stock.quote.latestPrice.toFixed(2)}</p>
-                      </div>
-                      <div className="col-md-4">
-                        <p><strong>Target Price: </strong> ${Number(stock.fundamentals['Target Price']).toFixed(2)}</p>
-                      </div>
-                      <div className="col-md-4">
-                        {stock.fundamentals['Target Price'] >= stock.quote.latestPrice ?
-                          <p className="badge badge-success py-1 px-1">{((1 - (stock.quote.latestPrice / stock.fundamentals['Target Price'])) * 100).toFixed(2) + "% Undervalued"}</p>
-                          : <p className="badge badge-danger py-1 px-1">{(((stock.quote.latestPrice / stock.fundamentals['Target Price']) - 1) * 100).toFixed(2) + "% Overvalued"}</p>}
-                      </div>
-                    </div>
-                    <div className="progress bg-dark">
-                      <div className="progress-bar" role="progressbar" style={{ width: Math.round(stock.quote.latestPrice / stock.quote.week52High * 100) + "%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">{"$" + stock.quote.latestPrice}</div>
-                    </div>
-                    <div className="row mt-2">
-                      <div className="col-md-4">
-                        <p><strong>Index: </strong>{stock.symbolData.exchangeName}</p>
-                      </div>
-                      <div className="col-md-4">
-                        <p><strong>P/E: </strong>{stock.quote.peRatio}</p>
-                      </div>
-                      <div className="col-md-4">
-                        <p><strong>Profit Margin: </strong>{Number(stock.fundamentals['Profit Margin (%)']).toFixed(2)}%</p>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-4">
-                        <p><strong>Market Cap: </strong>${(stock.quote.marketCap / 1000000000).toFixed(2)} billion</p>
-                      </div>
-                      <div className="col-md-4">
-                        <p><strong>Debt/Equity: </strong>{(stock.fundamentals['Debt/Eq'])} </p>
-                      </div>
-                      <div className="col-md-4">
-                        <p><strong>Price-to-Sales: </strong>{(stock.fundamentals['P/S'])} </p>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-12">
-                        <p><strong>Price-to-Book: </strong>{(stock.fundamentals['P/B'])} </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <QuoteCard stock={stock} userID={userID} updatePortfolio={updatePortfolio} />
               )
               : ""
             }
